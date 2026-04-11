0cb758171b1627af8fd9f36d2742f8527b3b13bf
SudhakaranV17 <sudhakaranv17@gmail.com>
2026-04-11 23:09:20 +0530

    fix(build): updated frontend vite allowed host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port:8000,
        host: "0.0.0.0",
-      allowedHosts: ["localhost"],
+      allowedHosts: ["*"],
       proxy: {
         "/api": {
           target: env.VITE_BASE_URL,
